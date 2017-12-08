f559db8bef89802ce38ee309deee540d75a394cd
Miguel Fdez <miguel.fercre@gogotech.hk>
2017-12-08 14:43:37 +0000

    Added algorithm parameters to settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -5,3 +5,7 @@ load_dotenv(find_dotenv())
 
 DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
 NUM_WORKERS = int(os.environ.get("NUM_WORKERS"))
+
+SAMP_RATE = int(os.environ.get("SAMP_RATE", 20))
+N_SMOOTH = int(os.environ.get("N_SMOOTH", 20))
+Z_THRESHOLD = int(os.environ.get("Z_THRESHOLD", 6))
