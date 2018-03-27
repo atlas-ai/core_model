755ee8e77c4ce0e051e86c1b8879567be4a46f5b
Miguel Fdez <miguel.fercre@gogotech.hk>
2018-03-27 14:58:13 +0000

    Added call to `read_param()` when the worker is launched so that the input parameter files are loaded once before the algorithm is executed

--- pipeline_processor/worker.py
+++ pipeline_processor/worker.py
@@ -8,7 +8,7 @@ from enum import Enum
 from multiprocessing import Process
 from sqlalchemy import create_engine
 from pipeline_processor import replay
-from work_flow import execute_algorithm, track_summary
+from work_flow import execute_algorithm, track_summary, read_param
 from pipeline_processor.utils import connect_db, get_detected_events_for_track, get_measurements
 
 
@@ -35,6 +35,13 @@ class Worker(Process):
     def run(self):
         print('WORKER STARTED')
 
+        cali_param, evt_param, acc_param, param_rtt, param_ltt, param_utn, param_lcr, param_lcl, param_acc, code_sys = \
+            read_param(cali_file=settings.CALI_FILE, evt_det_file=settings.EVT_DET_FILE,
+                       acc_det_file=settings.ACC_DET_FILE, rtt_eva_file=settings.RTT_EVA_FILE,
+                       ltt_eva_file=settings.LTT_EVA_FILE, utn_eva_file=settings.UTN_EVA_FILE,
+                       lcr_eva_file=settings.LCR_EVA_FILE, lcl_eva_file=settings.LCL_EVA_FILE,
+                       acc_eva_file=settings.ACC_EVA_FILE, code_file=settings.CODE_FILE)
+
         for data in iter(self.queue.get, None):
             data = json.loads(data)
             payload_data = data['payload']['data']
@@ -58,12 +65,10 @@ class Worker(Process):
             df_data = df['data'].apply(lambda x: pd.Series(x))
 
             try:
-                df_sum = execute_algorithm(raw_data=df_data, cali_param=settings.CALI_FILE,
-                                           evt_param=settings.EVT_DET_FILE, acc_param=settings.ACC_DET_FILE,
-                                           param_rtt=settings.RTT_EVA_FILE, param_ltt=settings.LTT_EVA_FILE,
-                                           param_utn=settings.UTN_EVA_FILE, param_lcr=settings.LCR_EVA_FILE,
-                                           param_lcl=settings.LCL_EVA_FILE, param_acc=settings.ACC_EVA_FILE,
-                                           samp_rate=settings.SAMP_RATE, n_smooth=settings.N_SMOOTH,
+                df_sum = execute_algorithm(raw_data=df_data, cali_param=cali_param, evt_param=evt_param,
+                                           acc_param=acc_param, param_rtt=param_rtt, param_ltt=param_ltt,
+                                           param_utn=param_utn, param_lcr=param_lcr, param_lcl=param_lcl,
+                                           param_acc=param_acc, samp_rate=settings.SAMP_RATE, n_smooth=settings.N_SMOOTH,
                                            tn_thr=settings.TN_THR, lc_thr=settings.LC_THR, acc_thr=settings.ACC_THR,
                                            l1_thr=settings.L1_THR, l2_thr=settings.L2_THR, l3_thr=settings.L3_THR,
                                            l4_thr=settings.L4_THR, device_id=settings.DEVICE_ID,
